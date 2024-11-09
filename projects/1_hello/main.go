0b1bffef13d954090a104008f15268357aa30253
Men-fish <yugleb210@gmail.com>
2024-11-09 17:23:14 +0300

    a

--- projects/1_hello/main.go
+++ projects/1_hello/main.go
@@ -1,7 +1,21 @@
 package main
 
-// здесь надо написать код
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/get" {
+		fmt.Fprintf(w, "Hello, web!")
+	} else {
+		http.NotFound(w, r)
+	}
+}
 
 func main() {
-	// и здесь тоже
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
+
+// curl http://localhost:8080/get
--- projects/2_query/main.go
+++ projects/2_query/main.go
@@ -1,9 +1,29 @@
 package main
 
-// здесь надо написать код
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func main() {
-	// и здесь тоже
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		http.Error(w, "Parameter 'name' is required", http.StatusBadRequest)
+		return
+	}
+
+	response := fmt.Sprintf("Hello, %s!", name)
+	w.Write([]byte(response))
 }
 
-// и тут тоже (если очень надо)
+func main() {
+	http.HandleFunc("/api/user", userHandler)
+
+	fmt.Println("Server is running on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
+
+// curl http://localhost:8080/api/user?name=Men-fish
